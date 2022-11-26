51714bf1857666de2b531bd29c53588c941c62b6
Griffins-sys254 <griffinesonyango@gmail.com>
2022-11-26 10:23:24 +0300

    project initials

---
+++ OTPproject/OTPveryfication/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .models import Profile
+import random
+from .helper import MessageHandler
+
+def home(request):
+        pass
+
+def register(request):
+        pass
+
+def otpVerify(request,uid):   
+        pass    
---
+++ README.md
@@ -0,0 +1,14 @@
+install twilio using
+    pip3 install django twilio
+
+create a django project
+    django-admin startproject projectname
+
+create app
+    python3 manage.py startapp appname
+
+add the appname to the installed apps in you djangoproject.settings
+
+include your appurls to your djangoproject.urls
+
+
